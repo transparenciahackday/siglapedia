910e8d9fd070b36d0a88ac4c68ac0cd29793886b
Ricardo Lafuente <r@sollec.org>
2013-07-13 16:06:43 +0100

    Activate admin, add URLs and initial page

--- siglapedia/urls.py
+++ siglapedia/urls.py
@@ -1,17 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'siglapedia.views.home', name='home'),
-    # url(r'^sig/', include('siglapedia.foo.urls')),
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='home'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
