2c4450afae7080e4e087397da401c27df167d0b6
Ricardo Lafuente <r@sollec.org>
2013-07-14 16:42:26 +0100

    Removed Static Root from global settings

--- siglapedia/settings.py
+++ siglapedia/settings.py
@@ -30,9 +30,6 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/media/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
