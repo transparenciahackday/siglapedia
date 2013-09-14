b151339f65b38ba5a569768e7b932dda55080b1d
Ricardo Lafuente <r@sollec.org>
2013-09-14 18:03:30 +0100

    Remove description field, it's a bit confusing ATM

--- acronyms/models.py
+++ acronyms/models.py
@@ -6,7 +6,7 @@ from django.forms import ModelForm
 class Acronym(models.Model):
     name = models.CharField("Sigla", help_text="A sigla que queres adicionar", max_length=30)
     definition = models.CharField("Definição", help_text="O significado exato da sigla", max_length=300)
-    description = models.TextField("Descrição", help_text="O que é esta entidade? (opcional) Descreve-a com uma frase. A Wikipédia ajuda!", max_length=5000, blank=True)
+    description = models.TextField("Descrição", help_text="O que é esta entidade? (opcional) Descreve-a com uma frase. A Wikipédia ajuda!", max_length=5000, blank=True, editable=False)
     link = models.URLField("Link", help_text="Link para obter mais informação sobre esta sigla (opcional). Nós gostamos de links da Wikipédia em vez de links institucionais, sempre que possível.", blank=True)
 
 class AddRequest(models.Model):
